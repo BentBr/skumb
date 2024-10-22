ed99539460cf62e017fc57fb564a9a58eaa933bc
Bent Brüggemann <bent.brueggemann@blackbit.de>
2024-10-23 00:00:26 +0200

    Removed not needed verbosity for lang

--- fe/src/stores/app.js
+++ fe/src/stores/app.js
@@ -6,10 +6,7 @@ export const useAppStore = defineStore('app', () => {
     const locale = useI18n()
 
     const changeLanguage = (lang) => {
-        console.log('Changing language to ', lang)
-
-        console.log('current language ', locale.locale.value)
-        if (!Object.values(LanguageConstant).includes(lang)) {
+         if (!Object.values(LanguageConstant).includes(lang)) {
             console.error(`Language ${lang} is not supported)`)
 
             return
