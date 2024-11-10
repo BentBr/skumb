19217161a7434660f2789de92aef02f391bd76c4
Bent Brüggemann <bent.brueggemann@blackbit.de>
2024-11-10 22:54:06 +0100

    New content for welcome

--- fe/src/components/TheWelcome.vue
+++ fe/src/components/TheWelcome.vue
@@ -12,176 +12,97 @@
         <template #icon>
             <DocumentationIcon />
         </template>
-        <template #heading>Documentation</template>
+        <template #heading>Skumb</template>
 
-        Vue’s
-        <a
-            href="https://vuejs.org/"
-            target="_blank"
-            rel="noopener"
-        >
-            official documentation
-        </a>
-        provides you with all information you need to get started.
+        <p>
+            Skumb is the idea of a simple chat which is anonymous, secure and volatile.
+        </p>
+        <p>
+            You can simply create a new chat without any registration and share the link with your friends, colleagues or family.
+        </p>
+        <p>
+            Every chat is end-to-end encrypted with latest and proven as secure algorithms.
+        </p>
+        <p>
+            The chat is volatile, which means that the chat is not stored anywhere and leaving the page or refreshing it will delete all the messages (current anonymous version).
+        </p>
     </WelcomeItem>
 
     <WelcomeItem>
         <template #icon>
             <ToolingIcon />
         </template>
-        <template #heading>Tooling</template>
+        <template #heading>Anonymous chat</template>
 
-        This project is served and bundled with
-        <a
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener"
-        >
-            Vite
-        </a>
-        . The recommended IDE setup is
-        <a
-            href="https://code.visualstudio.com/"
-            target="_blank"
-            rel="noopener"
-        >
-            VSCode
-        </a>
-        +
-        <a
-            href="https://github.com/johnsoncodehk/volar"
-            target="_blank"
-            rel="noopener"
-        >
-            Volar
-        </a>
-        . If you need to test your components and web pages, check out
-        <a
-            href="https://www.cypress.io/"
-            target="_blank"
-            rel="noopener"
-        >
-            Cypress
-        </a>
-        and
-        <a
-            href="https://on.cypress.io/component"
-            target="_blank"
-            rel="noopener"
-        >
-            Cypress Component Testing
-        </a>
-        .
-
-        <br />
-
-        More instructions are available in <code>README.md</code>.
+        <p>
+            You want to quickly share a private key? Some password? Or other sensitive information which you don't want to store anywhere? Use
+            <a
+                href="https://skumb.eu"
+                title="skumb chat"
+            >
+                Skumb
+            </a>
+            .
+            <br />
+            No questions asked, no data stored, no registration needed. No possibility to recover the chat, your data, your messages.
+        </p>
     </WelcomeItem>
 
     <WelcomeItem>
         <template #icon>
             <EcosystemIcon />
         </template>
-        <template #heading>Ecosystem</template>
+        <template #heading>Whistleblowing (coming soon)</template>
 
-        Get official tools and libraries for your project:
-        <a
-            href="https://pinia.vuejs.org/"
-            target="_blank"
-            rel="noopener"
-        >
-            Pinia
-        </a>
-        ,
-        <a
-            href="https://router.vuejs.org/"
-            target="_blank"
-            rel="noopener"
-        >
-            Vue Router
-        </a>
-        ,
-        <a
-            href="https://test-utils.vuejs.org/"
-            target="_blank"
-            rel="noopener"
-        >
-            Vue Test Utils
-        </a>
-        , and
-        <a
-            href="https://github.com/vuejs/devtools"
-            target="_blank"
-            rel="noopener"
-        >
-            Vue Dev Tools
-        </a>
-        . If you need more resources, we suggest paying
-        <a
-            href="https://github.com/vuejs/awesome-vue"
-            target="_blank"
-            rel="noopener"
-        >
-            Awesome Vue
-        </a>
-        a visit.
+        <p>
+            You are a company or a collective of journalists and you want a way to be connected securely and anonymously?
+            <br />
+            With Skumb you can provide an option to be contacted in anonymity and security.
+            Get information about breaches, leaks or other topics of need first-hand and have those properly secure stored and only accessible by you.
+        </p>
     </WelcomeItem>
 
     <WelcomeItem>
         <template #icon>
             <CommunityIcon />
         </template>
-        <template #heading>Community</template>
+        <template #heading>Chat without remorse (coming soon)</template>
 
-        Got stuck? Ask your question on
-        <a
-            href="https://chat.vuejs.org"
-            target="_blank"
-            rel="noopener"
-        >
-            Vue Land
-        </a>
-        , our official Discord server, or
-        <a
-            href="https://stackoverflow.com/questions/tagged/vue.js"
-            target="_blank"
-            rel="noopener"
-        >
-            StackOverflow
-        </a>
-        . You should also subscribe to
-        <a
-            href="https://news.vuejs.org"
-            target="_blank"
-            rel="noopener"
-        >
-            our mailing list
-        </a>
-        and follow the official
-        <a
-            href="https://twitter.com/vuejs"
-            target="_blank"
-            rel="noopener"
-        >
-            @vuejs
-        </a>
-        twitter account for latest news in the Vue world.
+        <p>
+            You want to communicate in a modern and secure way without the scratching itch of some company evaluating your messages or meta data?
+            <br />
+            Don't support blind data collection and surveillance. Use Skumb.
+            <br />
+            Don't support questionable data retention policies. Use Skumb.
+            <br />
+            Don't support the data economy. Use Skumb.
+            <br />
+            Don't support questionable oligarchies. Use Skumb.
+            <br />
+            Support privacy, security, anonymity and a shared love for human rights, dignity and a proper freedom of speech.
+            <a
+                href="https://skumb.eu"
+                title="skumb chat"
+            >
+                Use Skumb
+            </a>
+            .
+        </p>
     </WelcomeItem>
 
     <WelcomeItem>
         <template #icon>
             <SupportIcon />
         </template>
-        <template #heading>Support Vue</template>
+        <template #heading>Made with love in Rust and Vue (support coming soon)</template>
 
-        As an independent project, Vue relies on community backing for its sustainability. You can help us by
+        As an independent project, we are always open for support and some shared love.
         <a
-            href="https://vuejs.org/sponsor/"
+            href="mailto:shared-love@skumb.eu"
             target="_blank"
             rel="noopener"
         >
-            becoming a sponsor
-        </a>
-        .
+            Become a supporter
+        </a>.
     </WelcomeItem>
 </template>
